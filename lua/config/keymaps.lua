258d6a309bc6f5188e3506ceb193901ede14188b
Mikael Andersson <qubex2@gmail.com>
2023-07-16 20:43:33 +0200

    Updated

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -21,8 +21,8 @@ map("n", "ä", "$", {})
 map("v", "ä", "$", {})
 -- windows --
 -- buffers --
-map("n", "<tab>", ":bn<cr>", opts)
-map("n", "<s-tab>", ":bp<cr>", opts)
+-- map("n", "<tab>", ":bn<cr>", opts)
+-- map("n", "<s-tab>", ":bp<cr>", opts)
 -- misc --
 map("n", "<leader>sx", require("telescope.builtin").resume, { noremap = true, silent = true, desc = "Resume" })
 -- dap --
